ea2b7ff25c305d4e1e0adab2dedb212ea1fca3d9
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-11-29 16:22:04 -0500

    flask main application

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hello world")
+from flask import Flask, jsonify
+from routes.users import users
+
+app = Flask(__name__)
+
+
+@app.get('/ping')
+def ping():
+    return jsonify({'message': 'Pong!'})
+
+
+app.register_blueprint(users)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
